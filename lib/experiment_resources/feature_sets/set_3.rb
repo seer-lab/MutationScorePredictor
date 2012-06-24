bd766d0d2a0d8f148dd87558fbbe0e411d62665f
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2012-06-24 19:19:13 -0400

    Add tasks to perform cross-validation experiments
    Add:
     * Tasks to perform cross-validation experiments over all projects using the
       different feature sets
     * All the different feature sets and project configurations as resources

---
+++ lib/experiment_resources/feature_sets/set_3.rb
@@ -0,0 +1,87 @@
+def ignore_field(field)
+  if field == "id" || field == "project" || field == "run" || field == "class_name" ||
+    field == "method_name" || field == "occurs" || field == "usable" ||
+    field == "created_at" || field == "updated_at" || field == "tests_touched" ||
+
+    # Mutation Testing
+    field == "killed_mutants" ||
+    field == "covered_mutants" ||
+    field == "generated_mutants" ||
+    field == "mutation_score_of_covered_mutants" ||
+    field == "mutation_score_of_generated_mutants" ||
+
+    # Mutation Operators
+    field == "killed_no_mutation" ||
+    field == "total_no_mutation" ||
+    field == "killed_replace_constant" ||
+    field == "total_replace_constant" ||
+    field == "killed_negate_jump" ||
+    field == "total_negate_jump" ||
+    field == "killed_arithmetic_replace" ||
+    field == "total_arithmetic_replace" ||
+    field == "killed_remove_call" ||
+    field == "total_remove_call" ||
+    field == "killed_replace_variable" ||
+    field == "total_replace_variable" ||
+    field == "killed_absolute_value" ||
+    field == "total_absolute_value" ||
+    field == "killed_unary_operator" ||
+    field == "total_unary_operator" ||
+    field == "killed_replace_thread_call" ||
+    field == "total_replace_thread_call" ||
+    field == "killed_monitor_remove" ||
+    field == "total_monitor_remove" ||
+
+    # Class Metrics
+    field == "norm" ||
+    field == "nof" ||
+    field == "nsc" ||
+    field == "nom" ||
+    field == "dit" ||
+    field == "lcom" ||
+    field == "nsm" ||
+    field == "six" ||
+    field == "wmc" ||
+    field == "nsf" ||
+
+    # Method Metrics
+    field == "mloc" ||
+    field == "nbd" ||
+    field == "vg" ||
+    field == "par" ||
+
+    # Coverage
+    field == "lcov" ||
+    field == "ltot" ||
+    field == "lscor" ||
+    field == "bcov" ||
+    field == "btot" ||
+    field == "bscor" ||
+    field == "not" ||
+
+    # Accumulated Test Unit Metrics
+    field == "stmloc" ||
+    field == "atmloc" ||
+    field == "stnbd" ||
+    field == "atnbd" ||
+    field == "stvg" ||
+    field == "atvg" ||
+    field == "stpar" ||
+    field == "atpar" ||
+
+    # Accumulated Code Unit Metrics
+    # field == "smloc" ||
+    # field == "amloc" ||
+    # field == "snbd" ||
+    # field == "anbd" ||
+    # field == "svg" ||
+    # field == "avg" ||
+    # field == "spar" ||
+    # field == "apar" ||
+
+    field == "."
+    return true
+  else
+    return false
+  end
+end
---
+++ lib/rake_tasks/experiment.rb
@@ -0,0 +1,57 @@
+@experiment_runs = 10
+@experiment_resources_dir = "#{Dir.pwd}/lib/experiment_resources"
+
+desc "Perform cross validation using all feature sets on the 'all' project"
+task :cross_validation_all_features_experiment do
+
+  features = Dir.entries("#{@experiment_resources_dir}/feature_sets").select {|entry| !File.directory? File.join("#{@experiment_resources_dir}/feature_sets",entry) and !(entry =='.' || entry == '..') }
+  projects = Dir.entries("#{@experiment_resources_dir}/cross_validation").select {|entry| File.directory? File.join("#{@experiment_resources_dir}/cross_validation",entry) and !(entry =='.' || entry == '..') }
+
+  features.each do |feature|
+
+    FileUtils.cp("#{@experiment_resources_dir}/feature_sets/#{feature}", "#{@home}/lib/feature_sets.rb")
+
+    projects.each do |project|
+
+      # Ignore all other projects except the 'all' project
+      next if project != "all"
+
+      FileUtils.cp("#{@experiment_resources_dir}/cross_validation/#{project}/configuration.rb", "#{@home}/lib/rake_tasks/configuration.rb")
+      if File.exist?("#{@experiment_resources_dir}/cross_validation/#{project}/cross_validation_#{feature.chomp(".rb")}.txt")
+        FileUtils.rm("#{@experiment_resources_dir}/cross_validation/#{project}/cross_validation_#{feature.chomp(".rb")}.txt")
+      end
+      @experiment_runs.times do |i|
+        `time rake cross_validation >> #{@experiment_resources_dir}/cross_validation/#{project}/cross_validation_#{feature.chomp(".rb")}.txt`
+      end
+    end
+  end
+end
+
+desc "Perform cross validation using the three combined feature sets on all individual project"
+task :cross_validation_all_projects_experiment do
+
+  features = Dir.entries("#{@experiment_resources_dir}/feature_sets").select {|entry| !File.directory? File.join("#{@experiment_resources_dir}/feature_sets",entry) and !(entry =='.' || entry == '..') }
+  projects = Dir.entries("#{@experiment_resources_dir}/cross_validation").select {|entry| File.directory? File.join("#{@experiment_resources_dir}/cross_validation",entry) and !(entry =='.' || entry == '..') }
+
+  features.each do |feature|
+
+    # Ignore all features are not the combine sets
+    next if !feature.include?("combine")
+
+    FileUtils.cp("#{@experiment_resources_dir}/feature_sets/#{feature}", "#{@home}/lib/feature_sets.rb")
+
+    projects.each do |project|
+
+      # Ignore the 'all' project
+      next if project == "all"
+
+      FileUtils.cp("#{@experiment_resources_dir}/cross_validation/#{project}/configuration.rb", "#{@home}/lib/rake_tasks/configuration.rb")
+      if File.exist?("#{@experiment_resources_dir}/cross_validation/#{project}/cross_validation_#{feature.chomp(".rb")}.txt")
+        FileUtils.rm("#{@experiment_resources_dir}/cross_validation/#{project}/cross_validation_#{feature.chomp(".rb")}.txt")
+      end
+      @experiment_runs.times do |i|
+        `time rake cross_validation >> #{@experiment_resources_dir}/cross_validation/#{project}/cross_validation_#{feature.chomp(".rb")}.txt`
+      end
+    end
+  end
+end
